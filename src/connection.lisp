f62ee998e0ec29307551b903df9ef082d55f12dd
tamura shingo <tamura.shingo@gmail.com>
2019-11-03 10:32:38 +0900

    mod connection to sandbox

--- src/connection.lisp
+++ src/connection.lisp
@@ -7,7 +7,7 @@
 
 (defparameter +LOGIN-HOST+ "https://login.salesforce.com"
   "login host for production mode" )
-(defparameter +LOGIN-HOST-SANDBOX+ "http://test.salesforce.com"
+(defparameter +LOGIN-HOST-SANDBOX+ "https://test.salesforce.com"
   "login host for sandbox mode")
 (defparameter +PATH-PREFIX+ "services/async"
   "common prefix for accessing salesforce async api")
